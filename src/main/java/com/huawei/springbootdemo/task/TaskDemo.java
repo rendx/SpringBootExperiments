1d83bc6f3132e0e9313e17489666e013f862524d
Darren <rendx6@foxmail.com>
2018-11-22 23:42:04 +0800

    定时任务整合

--- src/main/java/com/huawei/springbootdemo/task/TaskDemo.java
+++ src/main/java/com/huawei/springbootdemo/task/TaskDemo.java
@@ -3,7 +3,6 @@ package com.huawei.springbootdemo.task;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -12,8 +11,25 @@ public class TaskDemo {
 
     private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd: HH:mm:ss");
 
-    @Scheduled(fixedRate = 2000)
-    public void executeTask(){
-        System.out.println("Running task "+ dateFormat.format(new Date()));
+    @Scheduled(fixedRate = 2000L)
+    public void executeTask() {
+        System.out.println("Running task " + dateFormat.format(new Date()));
+    }
+
+    @Scheduled(cron = "*/5 * * * * *")
+    public void executeTaskWithCronExpression() {
+        System.out.println("Running cron task " + dateFormat.format(new Date()));
+    }
+
+    @Scheduled(fixedDelay = 3000L)
+    public void executeTaskWithFixedDelay() throws InterruptedException {
+        Thread.sleep(4000L);
+        System.out.println("Running task with fixed delay " + dateFormat.format(new Date()));
+    }
+
+    @Scheduled(fixedDelayString = "3000")
+    public void executeTaskWithFixedDelayString() throws InterruptedException {
+        Thread.sleep(4000L);
+        System.out.println("Running task with fixed delay " + dateFormat.format(new Date()));
     }
 }
